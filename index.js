689f3bfa01085ba1eb133fffc57099fd94a200ca
lithium-03 <nidhin.m3gtr@gmail.com>
2015-04-07 13:34:12 +0530

    app.use(compress());

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var app = express();
 app.set('port', (process.env.PORT || 5000));
+var compress = require('compression');
+app.use(compress());
 app.use(express.static(__dirname + '/public'));
-app.listen(app.get('port'), function () { });
+app.listen(app.get('port'), function () {
+  console.log("Node app is running at localhost:" + app.get('port'));
+});
