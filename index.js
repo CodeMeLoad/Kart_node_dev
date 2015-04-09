78baaeb5b01e69287cd3521c4d4efb1133073cd3
lithium-03 <nidhin.m3gtr@gmail.com>
2015-04-09 13:41:26 +0530

    interpinging

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ app.set('port', (process.env.PORT || 5000));
 var compress = require('compression');
 app.use(compress());
 app.use(express.static(__dirname + '/public'));
+setInterval(function () {
+    http.get("http://www.teamkart.in/");
+    console.log('ping')
+}, 900000);
 app.listen(app.get('port'), function () {
   console.log("Node app is running at localhost:" + app.get('port'));
 });
