ab574091728ab82d9ae029d7ee3323d52e2d6edb
lithium-03 <nidhin.m3gtr@gmail.com>
2015-05-02 10:37:26 +0530

    one less line of code

--- index.js
+++ index.js
@@ -8,6 +8,4 @@ setInterval(function () {
     http.get("http://www.teamkart.in/");
     console.log('ping')
 }, 900000);
-app.listen(app.get('port'), function () {
-  console.log("Node app is running at localhost:" + app.get('port'));
-});
+app.listen(app.get('port'));
